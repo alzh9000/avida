ee38c0bc0712e8f1eeb815e7dbaf2f1ab9b96bc8
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-03-04 17:31:33 -0500

    python scripting

--- cbuild/work/getting_data.py
+++ cbuild/work/getting_data.py
@@ -1,7 +1,20 @@
+# python getting_data.py -o testing1.txt  # default command './avida' will be used
+# python getting_data.py ./some-command -o testing1.txt  # 'some-command' will be executed
+
+import argparse
 import subprocess
 
-command = ["./avida"]
-output_file = "testing1.txt"
+parser = argparse.ArgumentParser(
+    description="Execute a command and redirect output to a file"
+)
+parser.add_argument(
+    "command", nargs="*", default="./avida", help="the command to execute"
+)
+parser.add_argument(
+    "-o", "--output", metavar="FILE", help="the output file name", required=True
+)
 
-with open(output_file, "w") as f:
-    subprocess.run(command, stdout=f)
+args = parser.parse_args()
+
+with open(args.output, "w") as f:
+    subprocess.run(args.command, stdout=f)
