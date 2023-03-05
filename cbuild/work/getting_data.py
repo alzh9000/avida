a0bca5ebc457510b2fadd37cbd8b3a0865176c74
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-03-05 17:59:42 -0500

    edit environment.cfg success

--- cbuild/work/getting_data.py
+++ cbuild/work/getting_data.py
@@ -1,5 +1,6 @@
 # python getting_data.py -o testing1.txt  # default command './avida' will be used
 # python getting_data.py ./some-command -o testing1.txt  # 'some-command' will be executed
+values = {'NOT': 2.0, 'NAND': 1.0, 'AND': 2.0, 'ORN': 2.0, 'OR': 3.0, 'ANDN': 3.0, 'NOR': 4.0, 'XOR': 4.0, 'EQU': 5.0}
 
 import argparse
 import subprocess
@@ -11,32 +12,10 @@ parser.add_argument(
     "command", nargs="*", default="./avida", help="the command to execute"
 )
 parser.add_argument(
-    "-o", "--output", metavar="FILE", help="the output file name", required=True
+    "-o", "--output", metavar="FILE", help="the output file name", required=False
 )
 
 args = parser.parse_args()
-
-with open('environment.cfg', 'r') as f:
-    contents = f.read()
-
-# Split the contents of the file into individual lines
-lines = contents.split('\n')
-
-# Create an empty dictionary to store the process:value for each reaction
-processes = {}
-
-# Loop through each line in the file
-for line in lines:
-    if line.startswith('REACTION'):
-        parts = line.split()
-        print(parts)
-        value = float(parts[3].split('=')[1].split(':')[0])
-        name = parts[1]
-        processes[name] = value
-
-# Print out the dictionary
-print(processes)
-
 
 with open('environment.cfg', 'r') as f:
     contents = f.read()
@@ -49,18 +28,17 @@ for i, line in enumerate(lines):
     if line.startswith('REACTION'):
         parts = line.split()
         value = float(parts[3].split('=')[1].split(':')[0])
-        new_value = value * 2
-        lines[i] = line.replace(f'value={value}', f'value={new_value}')
+        lines[i] = line.replace(f'value={value}', f'value={values[parts[1]]}')
 
 # Join the modified lines back together
 contents = '\n'.join(lines)
 
 # Write the modified contents to a new file
-with open('new_setup.txt', 'w') as f:
+with open('environment.cfg', 'w') as f:
     f.write(contents)
 
 
-# with open(args.output, "w") as f:
+# with open("229r/" + args.output, "w") as f:
 #     subprocess.run(args.command, stdout=f)
 
 # TODO: figure out how to get Python to modify the values in environment.cfg, or have Python create a new environment.cfg file in this directory to replace the existing one, then execute the command.
