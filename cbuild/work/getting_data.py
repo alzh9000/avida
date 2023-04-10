323235548558e0d91e2685b4af8e0baba8e8d34d
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-04-10 12:19:31 -0400

    test data file

--- cbuild/work/getting_data.py
+++ cbuild/work/getting_data.py
@@ -3,6 +3,8 @@
 
 # python getting_data.py -o testing1.txt  # default command './avida' will be used
 # python getting_data.py ./some-command -o testing1.txt  # 'some-command' will be executed
+# ALBERT: I'm always going to run this with the command './avida' so I don't need to specify it (no other commands to run). Also, at this point I automatically name the file based on the datetime and configs, so don't need to include file name either. So can just do "python getting_data.py" and it will run the command './avida' and save the output to a file named based on the datetime and configs.
+# -o filename is appended to end of log file, so still useful if you want to specifically mark some name for the logfile.
 values = {'NOT': 3.0, 'NAND': 3.0, 'AND': 2.0, 'ORN': 2.0, 'OR': 3.0, 'ANDN': 3.0, 'NOR': 4.0, 'XOR': 4.0, 'EQU': 5.0}
 xy = {'x': 10, 'y': 10}
 
@@ -27,7 +29,10 @@ experiment_start_time_string = time.strftime(
         )
 log_file_name = "229r/" + str(xy) +"_" + str(values) + f"_{experiment_start_time_string}.txt"
 
-log_file_name = "229r/task_date_" + str(xy) +"_" + str(values) + f"_{experiment_start_time_string}.txt"
+log_file_name = "229r/task_" + str(xy) +"_" + str(values) + f"_date_{experiment_start_time_string}.txt"
+
+if args.output:
+    log_file_name +=  str(args.output)
 
 
 with open('dat_filenames.txt', 'w') as f:
