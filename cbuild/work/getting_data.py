8fea6ab6cd05d9be175397dc6ea0e27c69010283
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-04-11 18:32:36 -0400

    tweak rewards

--- cbuild/work/getting_data.py
+++ cbuild/work/getting_data.py
@@ -8,8 +8,8 @@
 # ALBERT: I'm always going to run this with the command './avida' so I don't need to specify it (no other commands to run). Also, at this point I automatically name the file based on the datetime and configs, so don't need to include file name either. So can just do "python getting_data.py" and it will run the command './avida' and save the output to a file named based on the datetime and configs.
 # -o filename is appended to end of log file, so still useful if you want to specifically mark some name for the logfile.
 # DEFAULT_values = {'NOT': 1.0, 'NAND': 1.0, 'AND': 2.0, 'ORN': 2.0, 'OR': 3.0, 'ANDN': 3.0, 'NOR': 4.0, 'XOR': 4.0, 'EQU': 5.0}
-values = {'NOT': 1.0, 'NAND': 1.0, 'AND': 2.0, 'ORN': 2.0, 'OR': 3.0, 'ANDN': 3.0, 'NOR': 4.0, 'XOR': 4.0, 'EQU': 5.0}
-xy = {'x': 10, 'y': 10}
+values = {'NOT': 1.0, 'NAND': 1.0, 'AND': 2.0, 'ORN': 2.0, 'OR': 4.0, 'ANDN': 4.0, 'NOR': 5.0, 'XOR': 5.0, 'EQU': 8.0}
+xy = {'x': 20, 'y': 20}
 
 import argparse
 import subprocess
